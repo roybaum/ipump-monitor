0dff8a5bab77ba1f26e2d4211da512a12cbd58cf
Roy Baum <royboybaum@gmail.com>
2026-03-17 13:38:56 -0500

    browser tab text change

--- ipump_monitor.py
+++ ipump_monitor.py
@@ -544,7 +544,7 @@ HTML = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Operational Log - iPump Monitor</title>
+    <title>iPump Op Log</title>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <style>
         * {
